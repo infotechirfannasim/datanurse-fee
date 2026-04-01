370ec926486277ebbee51c18357b8aaf5a2ce24f
abdulhaikhan110 <abdul.haikhan@infotechgroup.com>
2026-04-01 16:20:44 +0500

    fixes

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -2,13 +2,13 @@
 import {Component, inject} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
-import {Router, RouterLink} from '@angular/router';
+import {Router} from '@angular/router';
 import {AuthService} from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, FormsModule, RouterLink],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: 'login.component.html',
   styleUrls: ['login.component.scss'],
 })
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import {Component, ElementRef, HostListener, inject, signal, ViewChild} from '@angular/core';
+import {Component, ElementRef, inject, signal, ViewChild} from '@angular/core';
 import {RouterLink, RouterLinkActive} from '@angular/router';
 import {AuthService} from "../../../core/services/auth.service";
 import {User} from "../../../core/models/user.model";
@@ -20,23 +20,8 @@ export class SidebarComponent {
   private authService = inject(AuthService);
   menuOpen: boolean = false;
   protected readonly getUserInitials = getUserInitials;
-  @ViewChild('menuContainer') menuContainer!: ElementRef;
 
   userInfo = signal<User | null>(null);
-
-  menuItems: MenuItem[] = [
-    {
-      label: 'Logout',
-      action: () => this.authService.logout()
-    }
-  ];
-
-  @HostListener('document:click', ['$event'])
-  onClickOutside(event: Event): void {
-    if (!this.menuContainer.nativeElement.contains(event.target)) {
-      this.closeMenu();
-    }
-  }
 
   constructor() {
     this.authService.currentUser$.subscribe((user: any) => {
