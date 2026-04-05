616fff2261434a0760d606c05e21e32d79de5227
Irfan Nasim <irfan.nasim@infotechgroup.com>
2026-04-05 17:17:38 +0500

    various fixes

--- src/app/pages/auth/forgot-password/forgot-password.component.ts
+++ src/app/pages/auth/forgot-password/forgot-password.component.ts
@@ -1,52 +1,52 @@
 import {Component} from '@angular/core';
 import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
-import {Router, RouterLink} from '@angular/router';
+import {Router} from '@angular/router';
 import {HttpErrorResponse} from "@angular/common/http";
 import {AuthService} from "../../../core/services/auth.service";
 import {ForgotPasswordRequest} from "../../../core/models/user.model";
 
 @Component({
-  selector: 'app-forgot-password',
-  standalone: true,
-  imports: [ReactiveFormsModule, RouterLink],
-  templateUrl: './forgot-password.component.html',
-  styleUrls: ['../login/login.component.scss']
+    selector: 'app-forgot-password',
+    standalone: true,
+    imports: [ReactiveFormsModule],
+    templateUrl: './forgot-password.component.html',
+    styleUrls: ['../login/login.component.scss']
 })
 export class ForgotPasswordComponent {
 
-  forgotForm = this.fb.group({
-    email: ['', [Validators.required, Validators.email]]
-  });
-
-  isLoading = false;
-  message = '';
-  error = '';
-
-  constructor(
-      private fb: FormBuilder,
-      private authService: AuthService,
-      private router: Router
-  ) {
-  }
-
-  onSubmit() {
-    if (this.forgotForm.invalid) return;
-
-    this.isLoading = true;
-    this.error = '';
-    this.message = '';
-    const payload: ForgotPasswordRequest = {
-      email: this.forgotForm.value.email as string,
-    }
-    this.authService.forgotPassword(payload).subscribe({
-      next: () => {
-        this.message = 'If that email is registered, a reset link has been sent.';
-        this.isLoading = false;
-      },
-      error: (err: HttpErrorResponse) => {
-        this.error = err.error?.message || 'Something went wrong';
-        this.isLoading = false;
-      }
+    forgotForm = this.fb.group({
+        email: ['', [Validators.required, Validators.email]]
     });
-  }
+
+    isLoading = false;
+    message = '';
+    error = '';
+
+    constructor(
+        private fb: FormBuilder,
+        private authService: AuthService,
+        private router: Router
+    ) {
+    }
+
+    onSubmit() {
+        if (this.forgotForm.invalid) return;
+
+        this.isLoading = true;
+        this.error = '';
+        this.message = '';
+        const payload: ForgotPasswordRequest = {
+            email: this.forgotForm.value.email as string,
+        }
+        this.authService.forgotPassword(payload).subscribe({
+            next: () => {
+                this.message = 'If that email is registered, a reset link has been sent.';
+                this.isLoading = false;
+            },
+            error: (err: HttpErrorResponse) => {
+                this.error = err.error?.message || 'Something went wrong';
+                this.isLoading = false;
+            }
+        });
+    }
 }
