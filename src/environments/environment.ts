e74cc1f9e4d07d6cf7c0da71c6dab160983a9b59
abdulhaikhan110 <abdul.haikhan@infotechgroup.com>
2026-04-06 20:01:03 +0500

    various update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,5 +7,5 @@ export const environment = {
     api_context_path: '',
     api_access_client: 'drapp',
     api_secret_client: 'drapp',
-    turnstileSiteKey: '0x4AAAAAAC01sCJPOKQ8hylW',
+    turnstileSiteKey: '1x00000000000000000000AA',
 };
